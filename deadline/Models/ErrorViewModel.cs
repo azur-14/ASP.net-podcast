8ff7aa7dde0e153d31161e0b8b93803e3432a743
azur-14 <hieungan0906@gmail.com>
2024-09-22 11:21:04 +0700

    start

---
+++ deadline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deadline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
